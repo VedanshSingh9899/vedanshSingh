fcbf6a9ce9b88166587d4c09c01716a6b466756b
Sahil <sahilalvi769@gmail.com>
2025-07-07 19:36:06 +0530

    file is modification

--- takshshila-web-dev/index.js
+++ takshshila-web-dev/index.js
@@ -7,39 +7,36 @@ app.use(express.static(path.join(__dirname, 'src')));
 
 // Route for homepage
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src', 'Homes.html'));
+    res.sendFile(path.join(__dirname, 'src','html' ,'Homes.html'));
 });
 
 // Route for test series page
 app.get('/test', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src', 'test.html'));
+    res.sendFile(path.join(__dirname, 'src', 'html','test.html'));
 });
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src', 'notes.html'));
-});
-app.get('/question-bank', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src', 'question-bank.html'));
+    res.sendFile(path.join(__dirname, 'src', 'html','notes.html'));
 });
 app.get('/books', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src', 'books.html'));
+    res.sendFile(path.join(__dirname, 'src', 'html','books.html'));
 });
 app.get('/practice-paper', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src', 'practice_tab.html'));
+    res.sendFile(path.join(__dirname, 'src','html', 'practice_tab.html'));
 });
 app.get('/test-series', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src', 'test_series.html'));
+    res.sendFile(path.join(__dirname, 'src', 'html','test_series.html'));
 });
 app.get('/blogs', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src', 'blog.html'));
+    res.sendFile(path.join(__dirname, 'src', 'html','blog.html'));
 });
 app.get('/videos', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src', 'videos.html'));
+    res.sendFile(path.join(__dirname, 'src','html', 'videos.html'));
 });
 app.get('/contact', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src', 'contact.html'));
+    res.sendFile(path.join(__dirname, 'src','html', 'contact.html'));
 });
 app.get('/courses', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src', 'courses.html'));
+    res.sendFile(path.join(__dirname, 'src','html', 'courses.html'));
 });
 app.get('/english.pdf', (req, res) => {
     res.sendFile(path.join(__dirname, 'src', 'temporary', 'english.pdf'));
