6dd6c61cbd440be0bc917240bec8c3d046837a87
VedanshSingh9899 <vedanshpratapsingh@gmail.com>
2025-08-23 00:11:07 +0530

    test: signed commit with only one file

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-// Redirect the browser to the correct URL for the Starter page
 header("Location: takshshila-web-dev/backend/user_login_tracking_php_files/Starter.php");
 //header("location: /takshshila-web-dev/src/html/LoginMobile.html"); for checking any folder
-exit(); // Important: Stop the script after sending the redirect header
+exit();
 ?>
