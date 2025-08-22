6a20d4166b2a975ca4666ae23fc6eedfbb33d5dc
VedanshSingh9899 <vedanshpratapsingh@gmail.com>
2025-08-23 00:18:38 +0530

    test: signed commit with index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 header("Location: takshshila-web-dev/backend/user_login_tracking_php_files/Starter.php");
-//header("location: /takshshila-web-dev/src/html/LoginMobile.html"); for checking any folder
+//header("location: /takshshila-web-dev/src/html/LoginMobile.html");
+// 1 for server-side redirect 2 for local redirect
 exit();
 ?>
